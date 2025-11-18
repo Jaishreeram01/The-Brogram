5d9998ae5e69d3ee0a2fde7c91964d5e69f33a4c
iamjai <jaishreeram0107@gmail.com>
2025-11-18 13:31:31 +0530

    Completed until WorkoutCard

---
+++ Brogram/src/components/Layout.jsx
@@ -0,0 +1,25 @@
+export default function Layout(props) {
+
+    const { children } = props
+    const header = (
+        <header>
+            <h1 className="text-gradient">The Brogram</h1>
+            <p><strong>The 30 Simple workouts program</strong>
+            </p>
+        </header>
+    )
+    const footer = (
+        <footer>
+            <p>Built by <a href="https://www.jaishreeram.netlify.app" target="_blank">Jai Shree Ram</a> <br />Styled with <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a></p>
+        </footer>
+    )
+
+
+    return (
+        <>
+            {header}
+            {children}
+            {footer}
+        </>
+    )
+}
